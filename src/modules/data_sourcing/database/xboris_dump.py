ca7d5bd77331b9b394c229f154bee19917493741
MikroMan <subic.tine@gmail.com>
2019-02-15 09:44:57 +0100

    basic writing to db and caching IDs

--- src/modules/data_sourcing/database/xboris_dump.py
+++ src/modules/data_sourcing/database/xboris_dump.py
@@ -1,12 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, select
 from sqlalchemy import Column, String, BigInteger, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import logging
 
-db_string = "postgres://admin:donotusethispassword@aws-us-east-1-portal.19.dblayer.com:15813/compose"
-
-db = create_engine(db_string)
 base = declarative_base()
+
 
 class XBorISEvent(base):
     __tablename__ = 'ps_events'
@@ -15,25 +14,32 @@ class XBorISEvent(base):
     timestamp = Column(BigInteger)
     data = Column(JSON)
 
-Session = sessionmaker(db)
-session = Session()
 
-base.metadata.create_all(db)
+class PostgresConnector():
 
-# Create
-doctor_strange = XBorISEvent(title="Doctor Strange", director="Scott Derrickson", year="2016")
-session.add(doctor_strange)
-session.commit()
+    def __init__(self, username="postgres", password="", host="127.0.0.1", port=5432, db_name="postgres"):
+        user = username
+        if password != "":
+            user = user + ":" + password
+        self._logger = logging.getLogger(self.__class__.__name__ + "_logger")
+        self.conn_string = "postgres://{}@{}:{}/{}".format(user, host, port, db_name)
 
-# Read
-films = session.query(XBorISEvent)
-for film in films:
-    print(film.title)
+        self._logger.debug(self.conn_string)
 
-# Update
-doctor_strange.title = "Some2016Film"
-session.commit()
+        self.db = create_engine(self.conn_string)
 
-# Delete
-session.delete(doctor_strange)
-session.commit()
+        Session = sessionmaker(self.db)
+        self.session = Session()
+
+        base.metadata.create_all(self.db)
+
+    def store(self, dat):
+        for d in dat:
+            self.session.add(d)
+        self.session.commit()
+
+    def retrieve_ids(self, age=0):
+        query = select([XBorISEvent.event_id]).select_from(XBorISEvent).where(XBorISEvent.timestamp > age)
+        data = self.db.execute(query)
+
+        return [x[0] for x in data]
--- src/modules/data_sourcing/web/ps_grabber.py
+++ src/modules/data_sourcing/web/ps_grabber.py
@@ -5,22 +5,28 @@ import schedule
 import logging
 import datetime
 import json
+import modules.data_sourcing.database.xboris_dump
+from modules.data_sourcing.database.xboris_dump import XBorISEvent, PostgresConnector
 
 class PS_Grabber:
-    def __init__(self):
+    def __init__(self, db_connector):
         self._task = None
+        self.db_connector = db_connector
         self._logger = logging.getLogger(self.__class__.__name__ + "_logger")
-        c_handler = logging.StreamHandler()
-        c_handler.setLevel(logging.DEBUG)
 
         self._xboris_url = "https://edataex.posta.si/xboris/api/GetData"
 
         self._status_code = {
             200: "OK", 401: "AuthenticationError", 404: "NoData", 405: "InvalidArguments", 500: "ServerError"
         }
+        self.existing_event_ids = []
+        self.load_cache()
 
-    def schedule(self, period=1): #TODO change period
-        schedule.every(period).minutes.do(self._scrape)
+
+
+    def schedule(self, period=10): #TODO change period
+        self._scrape()
+        #schedule.every(period).seconds.do(self._scrape)
 
     def _scrape(self):
         try:
@@ -33,26 +39,38 @@ class PS_Grabber:
 
             req = requests.post(self._xboris_url, data=json.dumps(payload), headers=headers, verify=False)
 
-
-
             if req.status_code == 200:
                 data = json.loads(req.content)
-                self._logger.debug("Request OK, read {} records".format(len(data["xBorISData"])))
+
+                new_events = []
+                for event in data["xBorISData"]:
+                    if event['Itemid'] not in self.existing_event_ids:
+                        new_events.append(XBorISEvent(event_id=event['Itemid'], timestamp=int(time.time()), data=event))
+                        self.existing_event_ids.append(event['Itemid'])
+
+                self.db_connector.store(new_events)
             else:
                 self._logger.error("[{}] {} : {}".format(req.status_code, self._status_code[req.status_code], req.text))
         except Exception as e:
             self._logger.error(e)
 
+    def load_cache(self):
+        age = time.time() - 60*60*24 #last 24 hours
+
+        self.existing_event_ids = self.db_connector.retrieve_ids(age=age)
+
+
 
 if __name__ =='__main__':
 
     logging.basicConfig(level=logging.DEBUG)
+    db = PostgresConnector(host="localhost")
+    xb = PS_Grabber(db)
 
-    xb = PS_Grabber()
 
     xb.schedule()
 
-    while True:
-        time.sleep(1)
-        schedule.run_pending()
+    # while True:
+    #     time.sleep(10)
+    #     schedule.run_pending()
 
