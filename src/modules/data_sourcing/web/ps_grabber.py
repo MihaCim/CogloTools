0c96d8ff01ad4c81dd06db65f12f40f36e4b7d3c
MikroMan <subic.tine@gmail.com>
2019-02-20 12:24:53 +0100

    cleanup

--- src/modules/data_sourcing/web/ps_grabber.py
+++ src/modules/data_sourcing/web/ps_grabber.py
@@ -1,12 +1,13 @@
+import datetime
+import json
+import logging
 import time
 
 import requests
 import schedule
-import logging
-import datetime
-import json
-import modules.data_sourcing.database.xboris_dump
+
 from modules.data_sourcing.database.xboris_dump import XBorISEvent, PostgresConnector
+
 
 class PS_Grabber:
     def __init__(self, db_connector):
@@ -14,22 +15,19 @@ class PS_Grabber:
         self.db_connector = db_connector
         self._logger = logging.getLogger(self.__class__.__name__ + "_logger")
 
-        self._xboris_url = "https://edataex.posta.si/xboris/api/GetData"
+        self._data_url = "https://edataex.posta.si/xboris/api/GetData"
 
         self._status_code = {
             200: "OK", 401: "AuthenticationError", 404: "NoData", 405: "InvalidArguments", 500: "ServerError"
         }
-        self.existing_event_ids = []
+        self._existing_event_ids = []
         self._load_id_cache()
 
-
-
-    def schedule(self, period=10): #TODO change period
+    def schedule(self, period=10):  # TODO change period
         '''
         Initializes periodical web source polling
         :param period: How often to poll data (minutes), default is 10
         '''
-        #self._scrape()
 
         schedule.every(period).minutes.do(self._scrape)
 
@@ -42,43 +40,43 @@ class PS_Grabber:
                 'Authorization': "Basic SUpTWEJvcklTOnNhaHJoZ2V3XzQzN2Ryc2h3MzQ1NjM=",
             }
 
-            req = requests.post(self._xboris_url, data=json.dumps(payload), headers=headers, verify=False)
+            req = requests.post(self._data_url, data=json.dumps(payload), headers=headers, verify=False)
 
             if req.status_code == 200:
                 data = json.loads(req.content)
 
-                new_events = []
-                for event in data["xBorISData"]:
-                    if event['Itemid'] not in self.existing_event_ids:
-                        new_events.append(XBorISEvent(event_id=event['Itemid'], timestamp=int(time.time()), data=event))
-                        self.existing_event_ids.append(event['Itemid'])
-
-                self.db_connector.store(new_events)
+                self.save_new_events(data)
             else:
                 self._logger.error("[{}] {} : {}".format(req.status_code, self._status_code[req.status_code], req.text))
         except Exception as e:
             self._logger.error(e)
 
+    def save_new_events(self, data):
+        new_events = []
+        for event in data["xBorISData"]:
+            if event['Itemid'] not in self._existing_event_ids:
+                new_events.append(XBorISEvent(event_id=event['Itemid'], timestamp=int(time.time()), data=event))
+                self._existing_event_ids.append(event['Itemid'])
+        self.db_connector.store(new_events)
+
     def _load_id_cache(self):
         '''
         Loads recent (24 hours) event IDs from database for duplicate detection
         '''
-        age = time.time() - 60*60*24 #last 24 hours
+        age = time.time() - 60 * 60 * 24  # last 24 hours
 
-        self.existing_event_ids = self.db_connector.retrieve_ids(age=age)
+        self._existing_event_ids = self.db_connector.retrieve_ids(age=age)
 
 
-
-if __name__ =='__main__':
+if __name__ == '__main__':
 
     logging.basicConfig(level=logging.DEBUG)
+
     db = PostgresConnector(host="localhost")
     xb = PS_Grabber(db)
 
-
     xb.schedule()
 
-    # while True:
-    #     time.sleep(10)
-    #     schedule.run_pending()
-
+    while True:
+        time.sleep(30)
+        schedule.run_pending()
