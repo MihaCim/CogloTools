c1915c737ff962b75e985fa63e17dd75df0df910
matejko94 <matej.senozetnik@gmail.com>
2019-05-10 12:16:33 +0200

    sax parsing

---
+++ src/modules/create/readOsmPaths.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import xml.sax
+
+nodes = {}
+
+class MovieHandler(xml.sax.ContentHandler):
+
+
+
+    def startElement(self, name, attrs):
+        print("Element start: %s (%d attribute(s))" % (name, len(attrs)))
+
+        if name == "node":
+            node = Node()
+            if "id" in attrs and "lat" in attrs and "lon" in attrs:
+                node.addNode(attrs["id"], attrs["lat"], attrs["lon"])
+                nodes[attrs["id"]] = node
+        if name == "way":
+            for key in dict(attrs):
+                print(key + ", " + attrs[key])
+        #if name == "nd":
+        #    print(name)
+
+
+    def endElement(self, name):
+        print("Element end: %s" % name)
+
+
+#    def characters(self, data):
+#      print("Characters: %s" % data)
+
+
+
+
+class Node:
+
+    def addNode(self,id, lat, lon):
+        self.id = id
+        self.lat = lat
+        self.lon = lon
+
+
+
+if (__name__ == "__main__"):
+    # create an XMLReader
+    parser = xml.sax.make_parser()
+    # turn off namepsaces
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    # override the default ContextHandler
+    Handler = MovieHandler()
+    parser.setContentHandler(Handler)
+
+    parser.parse("stahovica.osm")
+
+    print(len(nodes))
+
