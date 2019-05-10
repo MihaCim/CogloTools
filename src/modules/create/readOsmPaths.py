1a6812b9d572167e4498bee2b0494fb63fcdb4e1
matejko94 <matej.senozetnik@gmail.com>
2019-05-10 20:41:24 +0200

    add parsing of ways/node

--- src/modules/create/readOsmPaths.py
+++ src/modules/create/readOsmPaths.py
@@ -3,13 +3,13 @@
 import xml.sax
 
 nodes = {}
+ways = []
+
 
 class MovieHandler(xml.sax.ContentHandler):
 
-
-
     def startElement(self, name, attrs):
-        print("Element start: %s (%d attribute(s))" % (name, len(attrs)))
+       # print("Element start: %s (%d attribute(s))" % (name, len(attrs)))
 
         if name == "node":
             node = Node()
@@ -17,30 +17,44 @@ class MovieHandler(xml.sax.ContentHandler):
                 node.addNode(attrs["id"], attrs["lat"], attrs["lon"])
                 nodes[attrs["id"]] = node
         if name == "way":
+            self.way = Way()
+            print(name)
             for key in dict(attrs):
                 print(key + ", " + attrs[key])
-        #if name == "nd":
-        #    print(name)
+        if name == "nd":
+            self.way.addNode(attrs["ref"])
+            for key in dict(attrs):
+             print(key + ", " + attrs[key])
+
 
 
     def endElement(self, name):
         print("Element end: %s" % name)
+        if name == "way":
+            ways.append(self.way)
 
 
-#    def characters(self, data):
-#      print("Characters: %s" % data)
+
+    def characters(self, data):
+      print("Characters: %s" % data)
 
 
 
 
 class Node:
 
-    def addNode(self,id, lat, lon):
+    def addNode(self, id, lat, lon):
         self.id = id
         self.lat = lat
         self.lon = lon
 
 
+class Way:
+    def __init__(self):
+        self.ids = []
+
+    def addNode(self, id):
+        self.ids.append(id)
 
 if (__name__ == "__main__"):
     # create an XMLReader
@@ -55,4 +69,5 @@ if (__name__ == "__main__"):
     parser.parse("stahovica.osm")
 
     print(len(nodes))
+    print(len(ways))
 
