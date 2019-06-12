7520399d5243d57e9e2d0c6e2c5017cec09ba3d5
matejko94 <matej.senozetnik@gmail.com>
2019-06-12 23:12:41 +0200

    refactoring and basic

---
+++ src/modules/create/parseOsm.py
@@ -0,0 +1,119 @@
+import xml.sax
+from src.modules.create.utils import utils
+import networkx as nx
+
+class OsmParsers(xml.sax.ContentHandler):
+    '''
+        OsmParsers parse OSM data to nodes and ways. Nodes are represents as dictionary in order to fast accesss to it.
+        Ways are list.
+    '''
+    def __init__(self):
+        self.nodes = {}  # nodes are save
+        self.ways = []
+        self.tmpWays = []
+        self.tags = {}
+        self.roads = {"motorway", "trunk", "primary", "secondary", "tertiary", "unclassified", "residential", "service",
+                      "living_street", "motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link",
+                      "motorway_junction"}
+
+    def startElement(self, name, attrs):
+        if name == "node":
+            node = Node()
+            if "id" in attrs and "lat" in attrs and "lon" in attrs:
+                node.addNode(int(attrs["id"]), float(attrs["lat"]), float(attrs["lon"]))
+                self.nodes[int(attrs["id"])] = node
+        if name == "way":
+            self.tmpWays = []
+            self.tags = {}
+            # for key in dict(attrs):
+            #    print(key + ", " + attrs[key])
+        if name == "nd":
+            self.tmpWays.append(attrs["ref"])
+        if name == "tag":
+            self.tags[attrs["k"]] = attrs["v"]
+
+
+    def endElement(self, name):
+        if name == "way":
+            if "highway" in self.tags and self.tags["highway"] in self.roads:
+                for i in range(1, len(self.tmpWays)):
+                    distance = 0
+                    way = Way()
+                    node1 = self.nodes[int(self.tmpWays[i - 1])]
+                    node2 = self.nodes[int(self.tmpWays[i])]
+                    way.addPath(int(self.tmpWays[i - 1]), int(self.tmpWays[i]))
+                    distance = utils.calcDistance(node1.lat, node1.lon, node2.lat, node2.lon)
+                    way.addDistance(distance)
+                    self.ways.append(way)
+                self.tags = {}
+                self.tmpWays = {}
+class OsmHandler(xml.sax.ContentHandler):
+
+
+    def __init__(self):
+        # create an XMLReader
+        parser = xml.sax.make_parser()
+        # turn off namepsaces
+        parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+        # override the default ContextHandler
+        handler = OsmParsers()
+        parser.setContentHandler(handler)
+
+
+        parser.parse("data/kamnik_export.osm")
+
+        self.ways = handler.ways
+        self.nodes = handler.nodes
+
+    def graphViz(self):
+        G = nx.Graph()
+        n = set()
+
+        for edge in self.ways:
+            l = edge.getAllNodes()
+            if l[0] not in n:
+                G.add_node(l[0], pos=(self.nodes[l[0]].lat, self.nodes[l[0]].lon))
+            if l[1] not in n:
+                G.add_node(l[1], pos=(self.nodes[l[1]].lat, self.nodes[l[1]].lon))
+            G.add_edge(l[0], l[1], weight=edge.distance)
+            n.add(l[0])
+            n.add(l[1])
+        return G
+
+
+class Node:
+
+    def __init__(self):
+        self.post = False
+        self.address = ""
+
+    def addNode(self, id, lat, lon):
+        self.id = id
+        self.lat = lat
+        self.lon = lon
+
+    def addPost(self, address):
+        self.post = True
+        self.address = address;
+
+    def getId(self):
+        return self.id
+
+
+class Way:
+
+    def __init__(self):
+        self.ids = ()
+
+    def addPath(self, id1, id2):
+        self.ids = (id1, id2)
+
+    def getNodes(self):
+        return (self.ids[0], self.ids[len(self.ids) - 1])
+
+    def getAllNodes(self):
+        return self.ids
+
+    def addDistance(self, distance):
+        self.distance = distance
--- src/modules/create/readOsmPaths.py
+++ src/modules/create/readOsmPaths.py
@@ -1,95 +1,10 @@
 #!/usr/bin/python3
 import sys
-import xml.sax
-import networkx as nx
-import matplotlib.pyplot as plt
 import csv
-from math import radians, cos, sin, asin, sqrt, atan2
 import math
-
-nodes = {}
-ways = []
-
-
-class OsmHandler(xml.sax.ContentHandler):
-    def __init__(self):
-        self.tmpWays = []
-        self.tags = {}
-        self.roads = {"motorway", "trunk", "primary", "secondary", "tertiary", "unclassified", "residential", "service",
-                 "living_street", "motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link", "motorway_junction"}
-    def startElement(self, name, attrs):
-        if name == "node":
-            node = Node()
-            if "id" in attrs and "lat" in attrs and "lon" in attrs:
-                node.addNode(int(attrs["id"]), float(attrs["lat"]), float(attrs["lon"]))
-                nodes[int(attrs["id"])] = node
-        if name == "way":
-            self.tmpWays = []
-            self.tags = {}
-            #for key in dict(attrs):
-            #    print(key + ", " + attrs[key])
-        if name == "nd":
-            self.tmpWays.append(attrs["ref"])
-        if name == "tag":
-            self.tags[attrs["k"]] = attrs["v"]
-
-
-    def endElement(self, name):
-        if name == "way":
-            if "highway" in self.tags and self.tags["highway"] in self.roads:
-                for i in range(1, len(self.tmpWays)):
-                    distance = 0
-                    way = Way()
-                    node1 = nodes[int(self.tmpWays[i - 1])]
-                    node2 = nodes[int(self.tmpWays[i])]
-                    way.addPath(int(self.tmpWays[i - 1]), int(self.tmpWays[i]))
-                    distance = calcDistance(node1.lat, node1.lon, node2.lat, node2.lon)
-                    way.addDistance(distance)
-                    ways.append(way)
-                self.tags = {}
-                self.tmpWays = {}
-          #  else:
-          #      print(self.tags)
-
-
-    #def characters(self, data):
-    #  print("Characters: %s" % data)
-
-class Node:
-
-    def __init__(self):
-        self.post = False
-        self.address = ""
-
-    def addNode(self, id, lat, lon):
-        self.id = id
-        self.lat = lat
-        self.lon = lon
-
-    def addPost(self, address):
-        self.post = True
-        self.address = address;
-
-    def getId(self):
-        return self.id
-
-class Way:
-
-    def __init__(self):
-        self.ids = ()
-
-    def addPath(self, id1, id2):
-        self.ids = (id1, id2)
-
-    def getNodes(self):
-        return (self.ids[0], self.ids[len(self.ids)-1])
-
-    def getAllNodes(self):
-        return self.ids
-
-    def addDistance(self, distance):
-        self.distance = distance
-
+from src.modules.create.utils import utils
+from src.modules.create import parseOsm
+import networkx as nx
 
 class Post:
     def __init__(self, address, latitude, longitude):
@@ -97,92 +12,121 @@ class Post:
         self.latitude = latitude
         self.longitude = longitude
 
-def drawGraph(waysL):
-    G = nx.Graph()
-    #color_map = []
+class PostHandler:
 
-    pos = {}
+    def __init__(self):
+        self.posts = []
+
+    def isNumber(self, str):
+        try:
+            return float(str)
+        except ValueError:
+            return None
+
+    def readPostalOffices(self):
+        ''' Postal offices are read from csv file and than added to array
+        '''
+        self.posts = []
+        with open('data/List of Postal Offices (geographical location).csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+
+            for row in csv_reader:
+                address = [row[13], row[14], row[15]]
+                if (self.isNumber(row[22]) != None and self.isNumber(row[23]) != None):
+                    post = Post(' '.join(address), self.isNumber(row[22]), self.isNumber(row[23]))
+                    self.posts.append(post)
+
+        return self.posts
+
+
+    def alignNodesAndPosts(self, nodesL):
+        '''
+        Align post offices with nodes
+
+        :param posts:
+        :param nodesL:
+        :return:
+        '''
+        posts = self.readPostalOffices()
+        postsNodes = []
+        for post in posts:
+            minDist = sys.maxsize
+            nodeKey = ""
+            for key, node in nodesL.items():
+                tmpDist = utils.calcDistance(post.latitude, post.longitude, node.lat, node.lon)
+                if minDist > tmpDist:
+                    minDist = tmpDist
+                    nodeKey = key
+
+            tmpNode = nodesL[nodeKey]
+            if utils.calcDistance(post.latitude, post.longitude, tmpNode.lat, tmpNode.lon) < 1:
+                print(nodeKey)
+                print(post.address)
+                print(utils.calcDistance(post.latitude, post.longitude, tmpNode.lat, tmpNode.lon))
+                postsNodes.append(nodeKey)
+                tmpNode.addPost(post.address)
+                nodesL[nodeKey] = tmpNode
+        return (nodesL, postsNodes)
+
+
+
+def drawGraph(algPostalWays, H, nodes):
+    G = nx.Graph()
+    n = set()
+
+    li = []
+    for edge in algPostalWays:
+        l= edge.getAllNodes()
+        if l[0] not in n:
+            G.add_node(l[0], pos=(nodes[l[0]].lat, nodes[l[0]].lon))
+        if l[1] not in n:
+            G.add_node(l[1], pos=(nodes[l[1]].lat, nodes[l[1]].lon))
+        G.add_edge(l[0], l[1], weight=edge.distance)
+        n.add(l[0])
+        n.add(l[1])
+        li.append(l[0])
+        li.append(l[1])
+
+        import matplotlib.pyplot as plt
+
+
+        #plt.figure(1)
+        pos = nx.get_node_attributes(H, 'pos')
+        nx.draw(H, pos=pos, node_size=1)
+        nx.draw_networkx_nodes(G, pos, nodelist=l, node_color='g')
+        nx.draw_networkx_edges(G, pos, nodelist=l, node_color='g')
+
+        #plt.figure(2)
+        #nx.draw(G)
+        plt.show()
+
+
+'''
     for edge in waysL:
         l= edge.getAllNodes()
+        if l[0] not in n:
+            G.add_node(l[0], pos=(nodes[l[0]].lat, nodes[l[0]].lon))
+        if l[1] not in n:
+            G.add_node(l[1], pos=(nodes[l[1]].lat, nodes[l[1]].lon))
         G.add_edge(l[0], l[1], weight=edge.distance)
-        pos[l[0]] = (nodes[l[0]],nodes[l[1]])
+        n.add(l[0])
+        n.add(l[1])
 
-    # use one of the edge properties to control line thickness
-    edgewidth = [d['weight'] for (u, v, d) in G.edges(data=True)]
 
-    d = nx.degree(G)
-    pos = nx.spring_layout(G, iterations=50)
     # rendering
     plt.figure(figsize=(8,8))
     plt.axis('off')
+    pos = nx.get_node_attributes(G, 'pos')
 
-    #nx.draw_networkx_nodes(G, pos, node_size=1, nodelist=d._nodes.keys())
-    nx.draw(G, nodelist=d._nodes.keys(), node_size = 1, pos = pos)
-    #nx.draw_networkx_labels(G, pos)
-    #nx.draw_networkx_edges(G, pos, width=edgewidth, )
+    #  plt.figure(2)
+    # nx.draw(H)
+
+    print(pos)
+    nx.draw(G, pos =pos, node_size = 1)
     plt.savefig("Graph.png", format="PNG")
     plt.show()
+'''
 
-def isNumber(str):
-    try:
-        return float(str)
-    except ValueError:
-        return None
-
-
-def calcDistance(lat1, lon1, lat2, lon2):
-
-    # Code coppied form: http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians
-    R = 6373.0
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-    distance = R * c
-    return distance
-
-
-def readPostalOffices():
-    posts = []
-    with open('data/List of Postal Offices (geographical location).csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-
-        for row in csv_reader:
-            address = [row[13], row[14], row[15]]
-            if (isNumber(row[22]) != None and isNumber(row[23]) != None):
-                post = Post(' '.join(address), isNumber(row[22]), isNumber(row[23]))
-                posts.append(post)
-
-    return posts
-
-def alignNodesAndPosts(posts, nodesL):
-
-    for post in posts:
-        minDist = sys.maxsize
-        nodeKey = ""
-        for key, node in nodesL.items():
-            tmpDist = calcDistance(post.latitude, post.longitude, node.lat, node.lon)
-            if minDist > tmpDist:
-                minDist = tmpDist
-                nodeKey = key
-
-        tmpNode = nodesL[nodeKey]
-        if calcDistance(post.latitude, post.longitude, tmpNode.lat, tmpNode.lon) < 1:
-            print(nodeKey)
-            print(post.address)
-            print(calcDistance(post.latitude, post.longitude, tmpNode.lat, tmpNode.lon))
-            tmpNode.addPost(post.address)
-            nodesL[nodeKey] = tmpNode
-    return nodesL
 
 def search_near_posts(nodes, edges, start_nodes):
     fronta = [(start_nodes, 0, [])]
@@ -213,7 +157,7 @@ def search_near_posts(nodes, edges, start_nodes):
                 results.append(current_element)
         fronta.append((current_element, current_sdistance, current_visited_points))
         print(fronta)
-        st = 0
+
         visited_nodes.add(current_element)
         visited_nodes.add(start_nodes)
         # deleting procedure id should be much more simple
@@ -242,68 +186,51 @@ def search_near_posts(nodes, edges, start_nodes):
         #time.sleep(2)
     print("aa")
     print(results)
-
-
-
-def writeDict(dict):
-    import json
-    json = json.dumps(dict)
-    f = open("dict.json", "w")
-    f.write(json)
-    f.close()
-def readDict(dict):
-    import json
-    f = open("dict.txt", "w")
-    f.write(str(dict))
-    f.close()
+    return results
 
 
 if (__name__ == "__main__"):
-    # create an XMLReader
-    parser = xml.sax.make_parser()
-    # turn off namepsaces
-    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 
-    # override the default ContextHandler
-    Handler = OsmHandler()
-    parser.setContentHandler(Handler)
+    osmHandler = parseOsm.OsmHandler()
+    G = osmHandler.graphViz()
 
-    parser.parse("data/test_export.osm")
+    roadNodes = osmHandler.nodes
+    roadWays = osmHandler.ways
+    postHandler = PostHandler()
+    (roadNodesAnotated, postsNodes) = postHandler.alignNodesAndPosts(roadNodes)
 
-    print(len(nodes))
-    print(len(ways))
-    posts = readPostalOffices()
-    nodesFiltered = {}
-    for way in ways:
-        for id in way.ids:
-            nodesFiltered[id] = nodes[id]
-
-
-    nodesC = alignNodesAndPosts(posts, nodesFiltered)
 
     nodesDict = {}
     edgesDict = {}
 
-    for key, node in nodesC.items():
+    for key, node in roadNodesAnotated.items():
         nodesDict[node.id] = {"post":node.post}
 
-    for key, node in nodesC.items():
+    for key, node in roadNodesAnotated.items():
         edgesDict[node.id] = {}
 
-    for way in ways:
+    for way in roadWays:
         tmpD = edgesDict[way.ids[0]]
         tmpD[way.ids[1]] = {"weight": way.distance}
         edgesDict[way.ids[0]] = tmpD
 
-    print(len(nodes))
-    print(len(nodesFiltered))
+    print(len(roadNodes))
     print(len(edgesDict))
-    search_near_posts(nodesDict, edgesDict, 2244725770)
-    #6326558776
-    #Vir Gubčeva ulica 12
+
+    #for n in nodesNode:
 
 
-    drawGraph(ways)
+    algPostalWays = []
+    for posts in postsNodes:
+        results = search_near_posts(nodesDict, edgesDict, posts)
+        for result in results:
+            tmp = parseOsm.Way()
+            tmp.addPath(result, posts)
+            tmp.addDistance(utils.calcDistance(roadNodes[result].lat, roadNodes[result].lon, roadNodes[posts].lat, roadNodes[posts].lon))
+            algPostalWays.append(tmp)
+
+
+    drawGraph(algPostalWays, G, roadNodesAnotated)
 
 
     
