62b4bc05b05ac6e0441640b2312d2072af8f1cfb
tinesubic <subic.tine@gmail.com>
2019-10-14 10:21:18 +0200

    spectral partitioning

---
+++ src/modules/partitioning/k_means_sphere.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+def discretesample(p,n):
+    return np.random.choice(len(p), n, p=p)
+
+def kMeansSphere(X, k):
+    n = X.shape[0]
+    d = X.shape[1]
+
+    def similarity(x1, x2):
+        norm_x1 = np.linalg.norm(x1)
+        norm_x2 = np.linalg.norm(x2)
+        return np.dot(x1, x2) / (norm_x1 * norm_x2)
+
+    def calc_centroid(X):
+        return np.mean(X, 0)
+
+    def get_min(a, prev_a):
+        diff = np.abs(a - prev_a)
+        diff[diff > 1] = 1
+
+        return np.sum(diff)
+
+    samples = np.random.randint(0, k)
+    C = X[samples, :]
+    C = np.zeros((k, d))  # ???
+    rands = np.random.randint(0, n)
+
+    C[0, :] = X[rands]
+
+    probs = np.zeros(n)
+
+    for centroidN in range(1, k):
+        for recN in range(0, n):
+            rec = X[recN, :]
+            nearest_dist = np.inf
+
+            for exist_centroidN in range(0, centroidN):
+                centroid = C[exist_centroidN, :]
+                clust_rec_sim = similarity(rec, centroid)
+                clust_rec_dist = 0.5 * (1 - clust_rec_sim)
+
+                if clust_rec_dist < nearest_dist:
+                    nearest_dist = clust_rec_dist
+            probs[recN] = nearest_dist * nearest_dist
+        norm_factor = 1.0 / np.sum(probs)
+        probs = probs * norm_factor
+
+        chosenN = discretesample(probs, 1)[0]
+        C[centroidN, :] = X[chosenN, :]
+
+        print('Chosen centroid {}, probability: {}, max probability: {} '.format(chosenN, probs[centroidN],
+                                                                                 probs.max()))
+
+    prev_assignment = np.zeros(n)
+    assignment = np.zeros(n)
+    assignment.fill(0)
+    prev_assignment.fill(0)
+
+    change = True
+
+    while change:
+        for recN in range(0, n):
+            xi = X[recN, :]
+
+            best_idx = -1
+            best_sim = np.NINF
+
+            for clustN in range(0, k):
+                sim = similarity(xi, C[clustN, :])
+
+                if sim > best_sim:
+                    best_idx = clustN
+                    best_sim = sim
+
+            assignment[recN] = best_idx
+
+        for clustN in range(0, k):
+            Yn = X[assignment == clustN, :]
+            C[clustN, :] = calc_centroid(Yn)
+
+        diff = get_min(assignment, prev_assignment)
+        change = np.sum(diff) > 0
+        prev_assignment = assignment
+        print('Diff', diff)
+
+
+    print("kMeans done")
+    return assignment
---
+++ src/modules/partitioning/spectral.py
@@ -0,0 +1,102 @@
+import numpy as np
+import scipy.io as sio
+from modules.partitioning.k_means_sphere import kMeansSphere
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+eps_rel = 1.5
+threshold = 1
+eps = threshold * eps_rel
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+shapes = ['.', 'v', '+', 'o', '*', 'p', '1', 'D']
+sizes = [
+    500,
+    500,
+    130,
+    110,
+    ];
+
+draw_sphere = False
+
+n = sum(sizes)
+k = 4
+
+A_orig = eps_rel * np.random.rand(n, n)
+#A_orig = sio.loadmat('A_orig.mat')['A_orig']
+print(A_orig.shape)
+
+prev_end = 0
+
+for clusterN in range(len(sizes)):
+    ni = sizes[clusterN]
+    A_orig[ni:(ni+prev_end), ni:(ni+prev_end)] = A_orig[ni:(ni+prev_end), ni:(ni+prev_end)] + threshold - 0.5 * eps
+
+    prev_end = prev_end + ni
+
+for i in range(n):
+    A_orig[i, i] = 0
+
+A = A_orig + eps * np.random.rand(n, n)
+A = A - np.diag(np.diag(A))
+
+A = 0.5 * (A + A.T)
+A_norm = A / np.sum(A, 1, keepdims=True)  # for broadcast fix
+
+L = np.eye(n) - A_norm
+
+del A_orig, A, A_norm
+
+[v, V] = np.linalg.eig(L)
+
+I = np.argsort(v)
+v = v[I]
+V = V[:, I]
+del I
+
+X = V[:, 0:k]
+Y = X[:, 1:]
+
+Y_mean = np.mean(Y, 0)
+norms = np.sqrt(np.sum(Y * Y, 1, keepdims=True))
+Y_norm = Y / norms
+
+assignment = kMeansSphere(Y_norm, k)
+
+d = Y.shape[1]  # column count
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+if d == 2:
+    pass
+elif d == 3:
+    offset = 0
+    for real_clustN in range(len(sizes)):
+        ni = sizes[real_clustN]
+        for clustN in range(k):
+            idxs_real_clust = np.arange(0, ni) + offset
+            I_real_clust = np.full(n, False)
+            I_real_clust[idxs_real_clust] = True
+            I_clust = assignment == clustN
+            I = np.logical_and(I_real_clust, I_clust)
+
+            clr = colors[real_clustN]
+            shape = shapes[real_clustN]
+
+            Yi = Y[I, :]
+            Yi_norm = Y_norm[I, :]
+
+            if draw_sphere:
+                 ax.scatter(Yi_norm[:, 0], Yi_norm[:, 1], Yi_norm[:, 2], c=clr)
+            else:
+                ax.scatter(Yi[:, 0], Yi[:, 1], Yi[:, 2], c=clr)
+
+        offset += ni
+
+    ax.scatter(Y_mean[0], Y_mean[1], Y_mean[2], marker='^')
+else:
+    print('Invalid dimension')
+
+plt.show()
+
+print("Done")
