c62eabc97ab65e888951b1621d9c38d068b68ddf
tinesubic <subic.tine@gmail.com>
2019-11-05 09:09:14 +0100

    V1 improved multiple tries spectral part

--- src/modules/partitioning/spectral.py
+++ src/modules/partitioning/spectral.py
@@ -1,8 +1,22 @@
 import numpy as np
 from modules.partitioning.k_means_sphere import k_means_sphere
 
+def cut_intensity(Q, part, n_part):
+    n_vert = Q.shape[0]
+    ones_k = np.ones((n_part, 1))
+    P = np.zeros((n_vert, n_part))
+    for partN in range(n_part):
+        P[:, partN] = part == partN
+
+    Q_p = P.T * Q * P
+    c = ones_k.T * Q_p * ones_k - np.trace(Q_p)
+    return c
+
+def cut_size(Q, part, n_part):
+    return cut_intensity((Q > 0).astype(dtype=int), part, n_part)
+
 def spectral_part(Q, k):
-    n = Q.shape[1]
+    n = Q.shape[0]
     ones_n = np.ones((n, 1))
 
     Q = 0.5 * (Q + Q.T)
@@ -34,4 +48,19 @@ def spectral_part(Q, k):
     total_tries = 10
     for _ in range(total_tries):
         part = k_means_sphere(Y_norm, k, 200)
-        return part, Y
+        cut_sum = cut_intensity(Q, part, k)
+        cut_edges = cut_size(Q, part, k)
+
+        avg_cut_intens = cut_sum / cut_edges
+
+        rnd_part = 1 + np.floor(k*np.random.rand((n,1)))
+        rnd_cut_sum = cut_intensity(Q, rnd_part, k)
+        avg_rnd_cut_intens = rnd_cut_sum / cut_edges
+        rnd_cut_edges = cut_size(Q, rnd_part, k)
+        mean_rnd_cut_intens = mean_rnd_cut_intens + (avg_rnd_cut_intens/total_tries)
+
+        if cut_sum < best_cut:
+            best_cut = cut_sum
+            best_part = part
+
+    return best_part, Y
