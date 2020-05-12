8e9dab9c63a878c58342a13780ee23bf23d5ffc2
matejko94 <matej.senozetnik@gmail.com>
2020-05-12 18:02:18 +0200

    clustering added

--- src/modules/utils/methods/methods.py
+++ src/modules/utils/methods/methods.py
@@ -1,35 +1,42 @@
 import json
-import pandas as pd
+import numpy as np
 from sklearn.cluster import KMeans
+import pandas as pd
 
 
-@staticmethod
 def elta_clustering(data):
-        #prepare df
-        dataDir = ("./")
-        df = pd.read_csv(dataDir + r"ELTA_All11.csv")
-        X = df.loc[:, ['UID', 'Geocode X', 'Geocode Y']]
+    print(data)
+    l = []
+    for el in data['orders']:
+        l.append([el['UUIDParcel'], "destination"] + el['destination'])
+        l.append([el['UUIDParcel'], "pickup"] + el['pickup'])
+    df = pd.DataFrame(l)
+    # run clustering
+    kmeans = KMeans(n_clusters=3)
+    kmeans.fit(df[[2, 3]])  # Compute k-means clustering.
+    centers = kmeans.cluster_centers_
+    df["labels"] = labels = kmeans.labels_
+    for index, row in df.iterrows():
+        print(index)
+        print(row)
+        data['orders'][index // 2][row[1]] = row['labels']
+    print(df)
 
-        #run clustering
-        kmeans = KMeans(n_clusters=3, init='k-means++')
-        kmeans.fit(X[X.columns[1:3]])  # Compute k-means clustering.
-        X['cluster_label'] = kmeans.fit_predict(X[X.columns[1:3]])
-        df["labels"] = labels = kmeans.labels_
-        virtual_nodes = kmeans.cluster_centers_
+    clos = {"useCase": "ELTA"}
+    clos_list = []
+    i = 0
+    for row in centers:
+        clos_list.append({
+            "uuid": i,
+            "lat": row[0],
+            "lon": row[1]
+        })
+        i = i + 1
+    clos["CLOS"] = clos_list
 
-        #print the clusters
-        #X.plot.scatter(x='Geocode X', y='Geocode Y', c=labels, s=10, cmap='viridis')
-        #plt.scatter(centers[:, 0], centers[:, 1], c='black', s=50, alpha=0.5)
-        response = df.to_json()
-        json.dump(my_details, response)
+    return data, clos
 
-        return(response)
 
-@staticmethod
 def elta_map_parcels(data):
-        print(data)
-    #map parcels to exisitng virtual nodes
-
-
-
-
+    print(data)
+# map parcels to exisitng virtual nodes
