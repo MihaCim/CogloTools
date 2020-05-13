d91b4fdeeb79e5aa33b5c6b9a450d4a0ba08ef10
Miha Cimperman <miha.cimperman@gmail.com>
2020-05-13 20:36:10 +0200

    parcel fix

--- src/modules/utils/structures/parcel.py
+++ src/modules/utils/structures/parcel.py
@@ -4,11 +4,11 @@ class Parcel:
     Target is a destination Node
     Default package volume for poc is 1, for easier UUID unpacking
     """
-    def __init__(self, uuid, target, volume, current_location, geolocation_pickup=None, geolocation_delivery=None, type=None):
+    def __init__(self, uuid, target, volume, current_location, pickup_location=None, destination_location=None, type=None):
         self.volume = volume
         self.target = target
         self.uuid = uuid
         self.current_location = current_location
+        self.pickup_location = pickup_location
+        self.destination_location = destination_location
         self.type = type
-        #self.geolocation_pickup = geolocation_pickup
-        #self.geolocation_delivery = geolocation_delivery
