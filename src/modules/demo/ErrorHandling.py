de6bc5aded0d1c94841a3eca7983fda9cafd4c5c
m.senozetnik <m.senozetnik@sportradar.com>
2020-11-18 18:13:39 +0100

    errors

---
+++ src/modules/demo/ErrorHandling.py
@@ -0,0 +1,68 @@
+
+class ErrorHandling:
+
+    @staticmethod
+    def check_messsages_correctnes(json):
+
+        pass
+
+
+    '''
+    checkCLOs
+
+
+    checkParcels
+
+    checkMessageStructure
+
+    checkIDs
+
+
+    checkLocations
+    organization:
+        -elta
+        -ps, hp
+    event_type ->
+       -border, crossBorder ()
+       -brokenVehicle
+       -adHock
+
+    parcels -> payweight == 1 :)
+    parcels -> destination -> checkiraj latitude longitude
+                -> eliti -> latitude/longitude so lahko do his
+
+
+    slo-cro
+        - urban (vozimo do his)
+        - backbone (vozimo od post) (crossboarder)-> pase na un file ....
+    elta
+        - pocekiraj ce je int .. oz number ce je paywight 1
+        - urban (vozimo do his)-> ne rabi pasat na lokacije
+        - backbone (do post) ->
+
+    Clos neglede na event type vedno ista struktura:
+    clos -> state -> parcels (zavrni ce ni parclov) -> list string lahko je empty
+                            -> location -> latitude, longitude, pa station
+
+                    -> parcel -> list vseh parcele object preveri lokacije, station pa vse ostalo
+                                    -> id stirng
+                                    -> source/destination definiran z 4 paramtri:)
+                                    -> organization
+
+    clos ->  id string
+                info ->organization
+            -> location -> country, latitude, longitude, station
+
+            -> organization  in country same :)
+            -> Organization isti na parclih ..
+            -> remaing plan -> steps notri
+
+    event -> ce je mora imeti event_type
+
+    -> request in organization (pri vseh parclih in clojih)
+
+    TODO FUTURE
+    -> event -> backbone, cross
+
+
+    '''
