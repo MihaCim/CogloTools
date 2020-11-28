0ee320181553f910d4c7627f1480e91bdaf8cfe0
m.senozetnik <m.senozetnik@sportradar.com>
2020-11-28 13:02:35 +0100

    add error hendling

--- src/modules/demo/ErrorHandling.py
+++ src/modules/demo/ErrorHandling.py
@@ -1,8 +1,11 @@
 from jsonschema import validate
 import json
 
+from src.modules.create_graph.config import config_parser
+
 
 class ErrorHandling:
+    slo_case = {'SLO', 'PS'}
 
     def check_organization(self, input_data):
         '''
@@ -22,6 +25,18 @@ class ErrorHandling:
                 if parcel['organization'] != 'ELTA':
                     raise ValueError("Wrong organization. Organization should be the same at the same message")
 
+        if input_data['organization'] == 'PS':
+            for item in input_data['event']['info']['item']:
+                if item['organization'] not in self.slo_case:
+                    raise ValueError("Wrong organization. Organization should be the same at the same message")
+
+            for clo in input_data['clos']:
+                if clo['info']['organization'] not in self.slo_case:
+                    raise ValueError("Wrong organization. Organization should be the same at the same message")
+
+            for parcel in input_data['parcels']:
+                if parcel['organization'] not in self.slo_case:
+                    raise ValueError("Wrong organization. Organization should be the same at the same message")
 
     def check_complited_plan(self, input_data):
         '''
@@ -30,7 +45,19 @@ class ErrorHandling:
 
         for clo in input_data['clos']:
             if 'completed_plan' not in clo['state']:
-                raise ValueError("Complited plan is missing")
+                raise ValueError("Completed plan is missing")
+
+    def check_locations(self, input_data):
+
+        if input_data['organization'] == "PS" or \
+           input_data['organization'] == "HP" :
+            use_case_graph = config_parser.ConfigParser()
+            use_case_graph.get_graph_path("SLO-CRO_urban")
+            f = open(config_parser.get_graph_path(use_case_graph), "r")
+            print(f)
+
+        if input_data['organization'] == "SLO-CRO":
+            pass
 
     def check_messages_correction(self, input_data):
         event = input_data['event']['event_type']
@@ -50,11 +77,16 @@ class ErrorHandling:
             validate(instance=input_data, schema=json_schema)
         #TODO missing daily_plan
         '''
+        #1.) Check the organization
         self.check_organization(input_data)
+
         self.check_complited_plan(input_data)
 
-        # 2.) daily plan -> complited plan prazn oz missing
-        # 3.) slo cro use case vsi locaisni (pri vsakem parslu in pri vsaki cloju) v fajlu grapf/data poklice z configa
+        self.check_locations(input_data)
+
+
+        # 3.) slo cro use case vsi locaisni (pri vsakem parslu in pri vsaki cloju)
+        # v fajlu grapf/data poklice z configa
         # get_csv_path(self, use_case):
         #         if use_case == "SLO-CRO_crossborder":
 
