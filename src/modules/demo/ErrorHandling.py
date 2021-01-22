8a59077dba71070b22f4720840cbccd28777140f
Miha Cimperman <miha.cimperman@gmail.com>
2021-01-22 14:17:14 +0100

    update I/O parser

--- src/modules/demo/ErrorHandling.py
+++ src/modules/demo/ErrorHandling.py
@@ -56,8 +56,8 @@ class ErrorHandling:
 
     def check_locations(self, input_data):
 
-        if input_data['organization'] == "PS" or \
-                input_data['organization'] == "HP":
+        if (input_data['organization'] == "PS" or \
+                input_data['organization'] == "HP") and (input_data["event"] == None):
             use_case_graph = ConfigParser()
             fObj = open(use_case_graph.get_csv_path("SLO-CRO_crossborder"))
 
@@ -147,7 +147,7 @@ class ErrorHandling:
 
         self.check_remaining_plan(input_data)
 
-        #self.check_locations(input_data)
+        self.check_locations(input_data)
 
         self.check_payweight(input_data)
         self.check_parcel_clos(input_data)
